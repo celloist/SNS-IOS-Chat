a82dfffded001d7954d1eda17ee5a6e79baf84de
japguele <japguele@avans.nl>
2015-03-08 04:08:23 -0700

    models toegevoegd. lijst van chats.

--- Sns chat IOS/Sns chat IOS/ViewController.swift
+++ Sns chat IOS/Sns chat IOS/ViewController.swift
@@ -9,18 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private let chatModel  = RestFull()
+    private let request  = Request()
+    private let chatModel = RestFull()
+    
+    @IBOutlet weak var label: UILabel!
+    private var customer: Customer?;
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
+        chatModel.getData("https://frozen-inlet-5594.herokuapp.com/customers", callback: {(success : Bool, data: [String:AnyObject]) in
+            if success {
+                
+            self.customer = self.request.getCustomers(data)[7];
+                self.label.text = self.customer?.name;
+            }
+
+        });
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       /*
         chatModel.getData("https://frozen-inlet-5594.herokuapp.com/customers", callback: {(success : Bool, data: [String:AnyObject]) in
             
             if success {
                 if let result = data["result"] as? NSDictionary {
+                   
+                    
+                    
+                    
+                    
+                    
+                    
                     if let payload  = result["data"] as? NSArray {
+                        
                         //Loop through all the customers
                         for rawItem in payload {
+                            
+                            
+                            
+                            
+                            
+                            
+                            
                             if let customer = rawItem as? NSDictionary{
                                 //retreive and loop through all the customers chats
                                 if let chats = customer["chats"] as? NSArray {
@@ -53,13 +93,22 @@ class ViewController: UIViewController {
                 println("Failed to gather data")
             }
         })
+*/
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        println("changescreen")
+        var dest : ChatsController = segue.destinationViewController as ChatsController
+         println("changescreen2")
+        dest.customer = self.customer
+         println("changescreen3")
+        
+    
+    }
 }
 
---
+++ Sns chat IOS/Sns chat IOS/customer.swift
@@ -0,0 +1,26 @@
+//
+//  customer.swift
+//  Sns chat IOS
+//
+//  Created by User on 06/03/15.
+//  Copyright (c) 2015 Mark Jan Kamminga. All rights reserved.
+//
+
+import Foundation
+
+class Customer : User{
+    
+   
+  //  var chats:[chat]
+    
+    init(_name:String,_id:String
+        //,_chats:[chat]
+        ){
+      
+     // chats = _chats
+        
+        super.init(_id:_id,_name:_name)
+        
+    }
+    
+}
