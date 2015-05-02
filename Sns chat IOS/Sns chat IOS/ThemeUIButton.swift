35916c65bef170f2c50ff53d945a53d641871aa3
Mark Jan <markjan@terra-it.com>
2015-05-02 09:16:38 +0200

    custom navigation controller, plist

---
+++ Sns chat IOS/Sns chat IOS/ThemeUIButton.swift
@@ -0,0 +1,21 @@
+//
+//  ThemeUIButton.swift
+//  Sns chat IOS
+//
+//  Created by Mark Jan Kamminga on 01-05-15.
+//  Copyright (c) 2015 Mark Jan Kamminga. All rights reserved.
+//
+
+import UIKit
+
+class ThemeUIButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
