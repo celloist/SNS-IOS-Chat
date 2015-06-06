5931d61fccf66200844d01c72feb6447efbc8ec3
japguele <japguele@avans.nl>
2015-06-06 08:05:22 -0700

    added faq

---
+++ Sns chat IOS/snsioschat/QuestionViewController.swift
@@ -0,0 +1,45 @@
+//
+//  QuestionViewController.swift
+//  Sns chat IOS
+//
+//  Created by User on 06/06/15.
+//  Copyright (c) 2015 nl.avans.aii.IIINMBD. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    var faqQuestion : FaqQuestion?;
+  
+    @IBOutlet weak var answer: UILabel!
+           @IBOutlet weak var question: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let fQuestion = faqQuestion?.question{
+            question.text = fQuestion
+        }
+        if let fAnwser = faqQuestion?.answer{
+            answer.text = fAnwser
+            
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
